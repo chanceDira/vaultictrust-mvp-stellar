41da41eb4e7b37ef97f0b844ae7052c1ece4ef96
0xJonaseb11 <sebejaz99@gmail.com>
2026-04-20 04:02:19 +0800

    [fix]: propose serialization during asset registry

---
+++ packages/soroban-contracts/target/debug/deps/soroban_spec-1080e156ea39426e.d
@@ -0,0 +1,8 @@
+/Users/0xjonaseb11/Documents/COLLABORATION/VAULTIC-STELLAR/packages/soroban-contracts/target/debug/deps/soroban_spec-1080e156ea39426e.d: /Users/0xjonaseb11/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/soroban-spec-23.5.3/src/lib.rs /Users/0xjonaseb11/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/soroban-spec-23.5.3/src/read.rs
+
+/Users/0xjonaseb11/Documents/COLLABORATION/VAULTIC-STELLAR/packages/soroban-contracts/target/debug/deps/libsoroban_spec-1080e156ea39426e.rlib: /Users/0xjonaseb11/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/soroban-spec-23.5.3/src/lib.rs /Users/0xjonaseb11/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/soroban-spec-23.5.3/src/read.rs
+
+/Users/0xjonaseb11/Documents/COLLABORATION/VAULTIC-STELLAR/packages/soroban-contracts/target/debug/deps/libsoroban_spec-1080e156ea39426e.rmeta: /Users/0xjonaseb11/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/soroban-spec-23.5.3/src/lib.rs /Users/0xjonaseb11/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/soroban-spec-23.5.3/src/read.rs
+
+/Users/0xjonaseb11/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/soroban-spec-23.5.3/src/lib.rs:
+/Users/0xjonaseb11/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/soroban-spec-23.5.3/src/read.rs:
---
+++ packages/soroban-contracts/target/debug/deps/vaultic_fractional_token-31c8abf2e437948d.d
@@ -0,0 +1,5 @@
+/Users/0xjonaseb11/Documents/COLLABORATION/VAULTIC-STELLAR/packages/soroban-contracts/target/debug/deps/vaultic_fractional_token-31c8abf2e437948d.d: contracts/fractional-token/src/lib.rs
+
+/Users/0xjonaseb11/Documents/COLLABORATION/VAULTIC-STELLAR/packages/soroban-contracts/target/debug/deps/libvaultic_fractional_token-31c8abf2e437948d.rmeta: contracts/fractional-token/src/lib.rs
+
+contracts/fractional-token/src/lib.rs:
